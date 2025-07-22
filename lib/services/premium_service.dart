bac45e83d5028488a1c0a1953190a8f057b464b2
ptphat03 <tanphatphan2003@gmail.com>
2025-07-22 11:41:18 +0700

    update

--- lib/services/premium_service.dart
+++ lib/services/premium_service.dart
@@ -1,12 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class PremiumService {
-  static Future<bool> isUserPremium() async {
-    final user = FirebaseAuth.instance.currentUser;
-    if (user == null) return false;
-
-    final doc = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
-    return doc.data()?['premium'] ?? false;
-  }
-}
+// class PremiumService {
+//   static Future<bool> isUserPremium() async {
+//     final user = FirebaseAuth.instance.currentUser;
+//     if (user == null) return false;
+//
+//     final doc = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+//     return doc.data()?['premium'] ?? false;
+//   }
+// }
